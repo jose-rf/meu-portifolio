f07e23e06d5cd31c13d15ebee5a6e4b9724ee87a
jose-rf <joserfranca13@gmail.com>
2024-03-24 11:33:31 -0300

    Atualização de CV e correção de bugs

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var menuPopup = document.querySelector('.menu-popup');
+    var menuState = localStorage.getItem('menuState');
+
+    // Verificar o estado do menu armazenado
+    if (menuState === 'open') {
+        menuPopup.classList.add('active');
+    } else {
+        menuPopup.classList.remove('active');
+    }
+});
+
 function clickMenu() {
     console.log('Clicou no menu');
     var menuPopup = document.querySelector('.menu-popup');
+
+    // Toggle do estado do menu no armazenamento local
+    if (menuPopup.classList.contains('active')) {
+        localStorage.setItem('menuState', 'closed');
+    } else {
+        localStorage.setItem('menuState', 'open');
+    }
+
+    // Atualizar a classe do menu pop-up
     menuPopup.classList.toggle('active');
 }
 
-function fecharMenu() {
-    console.log('Fechou o menu');
-    var menuPopup = document.querySelector('.menu-popup');
-    menuPopup.classList.remove('active');
-}
